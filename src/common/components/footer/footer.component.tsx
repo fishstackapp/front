61ae64653d5d4db1a9b077b71b63eb7f17581f08
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-12-15 00:55:12 +0200

    Add footer

---
+++ src/common/components/footer/footer.component.tsx
@@ -0,0 +1,23 @@
+import { FooterLink } from '../footer-link/footer-link.component';
+
+export const Footer = () => {
+  return (
+    <div className="bg-gray-900 px-12">
+      <div className="py-12">
+        <div className="mb-4 font-bold text-sm text-zinc-500">
+          <span className="uppercase">Контакти</span>
+        </div>
+        <div>
+          <ul>
+            <FooterLink href="tel:+380441234567">044 123 45 67</FooterLink>
+            <FooterLink href="mailto:info@fishstack.app">info@fishstack.app</FooterLink>
+          </ul>
+        </div>
+      </div>
+      <hr className="relative left-[-3rem] w-[calc(100%_+_6rem)] border-zinc-500" />
+      <div className="py-8">
+        <span className="font-semibold text-xl text-white">🐟 FishStack</span>
+      </div>
+    </div>
+  );
+};
