e99e586248a83a34f926437bebbdd507f9e93b26
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-12-30 19:03:19 +0200

    created input base

--- src/common/components/link/link.component.tsx
+++ src/common/components/link/link.component.tsx
@@ -1,16 +1,16 @@
-import { FC, PropsWithChildren } from "react"
+import { FC, PropsWithChildren } from 'react';
 
-interface LinkProps{
+interface LinkProps {
   url: string;
 }
 
-export const Link:FC<LinkProps & PropsWithChildren> = ({url, children}) => {
+export const Link: FC<LinkProps & PropsWithChildren> = ({ url, children }) => {
   return (
-    <a 
-      href={url} 
+    <a
+      href={url}
       className="relative font-semibold text-sm sm:text-base after:content-[''] after:block after:absolute after:w-full after:h-px hover:after:bg-blue-400"
-      >
+    >
       {children}
     </a>
-  )
-}
+  );
+};
