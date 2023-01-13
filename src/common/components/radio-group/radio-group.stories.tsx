97ef24a250c542a16ceb3724469eeef788ed876e
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2023-01-14 01:31:31 +0200

    add radio group

---
+++ src/common/components/radio-group/radio-group.stories.tsx
@@ -0,0 +1,49 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Controller, useForm } from 'react-hook-form';
+import { RadioGroup } from './radio-group.component';
+
+export default {
+  title: 'Form/Radio group',
+  component: RadioGroup,
+} as ComponentMeta<typeof RadioGroup>;
+
+const Template: ComponentStory<typeof RadioGroup> = args => <RadioGroup {...args} />;
+
+export const View = Template.bind({});
+View.args = {
+  options: [
+    { label: 'Готівка', value: 'cash' },
+    { label: 'Картка', value: 'card' },
+  ],
+};
+
+const YupTemplate: ComponentStory<typeof RadioGroup> = args => {
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      paymentType: 'cash',
+    },
+  });
+
+  const onSubmit = (values: any) => {
+    alert(JSON.stringify(values));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="paymentType"
+        control={control}
+        render={({ field }) => <RadioGroup {...args} {...field} />}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export const WithForm = YupTemplate.bind({});
+WithForm.args = {
+  options: [
+    { label: 'Готівка', value: 'cash' },
+    { label: 'Картка', value: 'card' },
+  ],
+};
