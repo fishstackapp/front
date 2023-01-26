da0ed35a89944e30ab4e864c9dfd1b6748c5bf55
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2023-01-26 20:30:56 +0200

    fix adaptive footer

--- src/modules/footer/components/footer.component.tsx
+++ src/modules/footer/components/footer.component.tsx
@@ -9,65 +9,56 @@ import { FooterAboutUs } from '../../../common/components/footer-about-us/footer
 
 export const Footer = () => {
   const dynamicLink = '_blank';
-  return (
-    <footer className="mt-auto flex flex-col items-center justify-center gap-6 bg-gray-900 p-8 sm:justify-between sm:gap-8 sm:p-12 lg:flex-row lg:items-start lg:justify-start lg:gap-0 lg:px-20">
-      <div className="w-full sm:flex sm:justify-around lg:justify-between">
-        <div className="flex flex-col items-center justify-center lg:flex-row lg:items-start lg:gap-6">
-          <div>
-            <Logo footer />
-          </div>
-          <div className="mb-6 mt-6 lg:m-0">
-            <ul className="flex flex-col items-center justify-center gap-2 lg:items-start">
-              <FooterLink href="tel:+380680707657">+38 068 07 07 657</FooterLink>
-              <FooterLink href="mailto:fishstackonline@gmail.com">
-                fishstackonline@gmail.com
-              </FooterLink>
-              <FooterAboutUs to="/payment">Доставка та оплата</FooterAboutUs>
-              <FooterAboutUs to="/turning">Повернення та обмін</FooterAboutUs>
-            </ul>
-          </div>
-        </div>
 
-        <div className="flex flex-col items-center justify-center  sm:justify-start lg:mr-auto lg:ml-auto lg:justify-start">
-          <div className="mb-2 sm:mb-6">
-            <p className="text-sm font-bold uppercase text-zinc-500 sm:text-base">Приєднуйтесь</p>
-          </div>
-          <div>
-            <ul className="flex gap-4">
-              <li>
-                <a
-                  className="flex h-11 w-11 items-center justify-center rounded-full bg-gray-700 transition-all hover:bg-blue-500"
-                  href="https://instagram.com/fishstack.online?igshid=YmMyMTA2M2Y="
-                  target={dynamicLink}
-                >
-                  <InstaIcon className="h-6 w-6 child-path:fill-white" />
-                </a>
-              </li>
-              <li>
-                <a
-                  className="flex h-11 w-11 items-center justify-center rounded-full bg-gray-700 transition-all hover:bg-blue-500"
-                  href="https://t.me/fishstack"
-                  target={dynamicLink}
-                >
-                  <TelegramIcon className="h-6 w-6 child-path:fill-white" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+  return (
+    <footer className="mt-auto flex flex-col flex-wrap items-center justify-center gap-6 bg-gray-900  p-8 sm:flex-row sm:justify-between sm:gap-14">
+      <div className="flex flex-col items-center gap-2 sm:items-start">
+        <Logo footer />
+        <ul className="text-center sm:text-start">
+          <FooterLink href="tel:+380680707657">+38 068 07 07 657</FooterLink>
+          <FooterLink href="mailto:fishstackonline@gmail.com">fishstackonline@gmail.com</FooterLink>
+        </ul>
       </div>
 
-      <div className="flex w-full max-w-lg flex-col items-center justify-center lg:items-start lg:justify-start">
-        <div className="mb-2">
-          <p className="text-sm font-bold uppercase text-zinc-500 sm:text-base">
-            Підпишіться на розсилку
-          </p>
-        </div>
-        <div className="flex w-full flex-col items-center justify-center lg:flex-row lg:items-start lg:justify-around lg:gap-3">
-          <div className="lg:w-full">
-            <Input placeholder="E-mail" type="email" fullWidth />
-          </div>
-          <div className="lg:mt-1">
+      <div className="flex flex-col items-center gap-2 sm:items-start">
+        <p className="text-sm font-bold uppercase text-zinc-500 sm:text-base">Про нас</p>
+        <ul className="text-center sm:text-start">
+          <FooterAboutUs to="/payment">Доставка та оплата</FooterAboutUs>
+          <FooterAboutUs to="/turning">Повернення та обмін</FooterAboutUs>
+        </ul>
+      </div>
+
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-sm font-bold uppercase text-zinc-500 sm:text-base">Приєднуйтесь</p>
+        <ul className="flex items-center justify-center gap-4">
+          <li>
+            <a
+              className="flex h-11 w-11 items-center justify-center rounded-full bg-gray-700 transition-all hover:bg-blue-500"
+              href="https://instagram.com/fishstack.online?igshid=YmMyMTA2M2Y="
+              target={dynamicLink}
+            >
+              <InstaIcon className="h-6 w-6 child-path:fill-white" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex h-11 w-11 items-center justify-center rounded-full bg-gray-700 transition-all hover:bg-blue-500"
+              href="https://t.me/fishstack"
+              target={dynamicLink}
+            >
+              <TelegramIcon className="h-6 w-6 child-path:fill-white" />
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mx-auto flex flex-col items-center gap-2 lg:mx-0">
+        <p className="text-sm font-bold uppercase text-zinc-500 sm:text-base">
+          Підпишіться на розсилку
+        </p>
+        <div className="flex w-full flex-col items-center justify-center sm:flex-row sm:items-start sm:justify-around sm:gap-3">
+          <Input placeholder="E-mail" type="email" fullWidth />
+          <div className="sm:mt-1">
             <Button>
               Підписатись <SubmitIcon width="2" className="h-6 w-6 child-path:fill-white" />
             </Button>
