826c8d5aed7d3ab29bb8cb8955ee427a37e2489e
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2023-02-02 19:30:26 +0200

    add scrollTo

--- src/common/components/footer-about-us/footer-about-us.component.tsx
+++ src/common/components/footer-about-us/footer-about-us.component.tsx
@@ -1,11 +1,18 @@
-import { FC, PropsWithChildren } from 'react';
-import { Link } from 'react-router-dom';
+import { FC, PropsWithChildren, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 interface FooterAboutUsProps {
   to: string;
 }
 
 export const FooterAboutUs: FC<FooterAboutUsProps & PropsWithChildren> = ({ to, children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+
   return (
     <li>
       <Link to={to} className="font-bold text-sm text-gray-400 hover:text-gray-300 hover:underline">{children}</Link>
